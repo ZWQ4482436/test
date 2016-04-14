8870e112138fe857e8f744f4567d6f5ffe3d2ea0
xiaomage <xiaomage@126.com>
2016-04-14 19:56:17 +0800

    初始化

---
+++ APPTest/APPTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APPTest
+//
+//  Created by zwq on 16/4/14.
+//  Copyright © 2016年 zwq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APPTest/APPTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APPTest
+//
+//  Created by zwq on 16/4/14.
+//  Copyright © 2016年 zwq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
